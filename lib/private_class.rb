aa88323efd07f4296446e6dadf58f6d33a5c844d
Egor Barakhov <egor.barakhov@flatstack.com>
2022-06-03 20:49:13 +0300

    Initial commit

---
+++ lib/private_class.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "private_class/version"
+
+module PrivateClass
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/private_class/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module PrivateClass
+  VERSION = "0.1.0"
+end
